fc7f0625789b9388cfd254329edd9ed495dea527
abr-ya <gamer05@list.ru>
2025-11-27 07:06:33 +0300

    init: Start Vite React + Tailwind + ESLint + Prettier + TanstackQuery + RadixUI Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-amber-300">Hello, world!</h1>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} alt="React logo" />
+      </a>
+    </div>
+  );
+};
+
+export default App;
