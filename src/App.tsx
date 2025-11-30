77719ef70df612d85f1f5f2d4240a2d79bdb0e6c
abr-ya <gamer05@list.ru>
2025-11-30 12:05:19 +0300

    feat: Update TanStack Query, add Provider and Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CityPage, HomePage } from "./pages";
 import { Layout } from "./components/";
 import { ThemeProvider } from "./providers/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const App = () => (
-  <BrowserRouter>
-    <ThemeProvider defaultTheme="dark">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/city/:cityName" element={<CityPage />} />
-        </Routes>
-      </Layout>
-    </ThemeProvider>
-  </BrowserRouter>
-);
+const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
